90a2e88388c2a06222fdd15d2fd672e31cc70658
brad <brad@brad.tw>
2019-11-26 09:58:44 +0800

    3

--- app/src/main/java/com/example/iiiandroid10/MainActivity.java
+++ app/src/main/java/com/example/iiiandroid10/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.iiiandroid10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private MyAdapter myAdapter;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+        listView = findViewById(R.id.listView);
+        initListView();
+        fetchRemoteData();
     }
+
+    private void fetchRemoteData(){
+
+    }
+
+    private void initListView(){
+        myAdapter = new MyAdapter();
+        listView.setAdapter(myAdapter);
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
 }
